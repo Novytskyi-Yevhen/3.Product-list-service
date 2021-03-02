9d1372d811694a05ef035185a09006a304e8a92d
Roman Supyk <roman.supyk@gmail.com>
2021-03-02 18:24:20 +0200

    Added interface IProductFilter, class ProductFilterService, DI

---
+++ Services/Interfaces/IProductFilter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace ProductsWithRouting.Services.Interfaces
+{
+    public interface IProductFilter
+    {
+        public int FilterId { get; set; }
+        public string FilterName { get; set; }
+        public IEnumerable<ProductsWithRouting.Models.Product> Filter();
+    }
+}
---
+++ Services/ProductFilterService.cs
@@ -0,0 +1,37 @@
+﻿using ProductsWithRouting.Models;
+using ProductsWithRouting.Services.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductsWithRouting.Services
+{
+    public class ProductFilterService : IProductFilter
+    {
+        private List<Product> products;
+
+        public int FilterId { get; set; }
+        public string FilterName { get; set; }
+
+        public ProductFilterService(Data data)
+        {
+            products = data.Products;
+        }
+
+        public IEnumerable<Product> Filter()
+        {
+            IEnumerable<Product> filtered = products;
+
+            if (FilterId != 0)
+            {
+                filtered = filtered.Where(p => p.Id == FilterId);
+            }
+
+            if (!string.IsNullOrEmpty(FilterName))
+            {
+                filtered = filtered.Where(p => p.Name == FilterName);
+            }
+
+            return filtered;
+        }
+    }
+}
